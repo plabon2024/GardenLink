e2c10e9f1370a5c727cb17fbefa9539601e9a865
plabon <shaeedplabon@gmail.com>
2025-06-27 11:27:07 +0600

    remove devider

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -4,18 +4,14 @@ import { Slide } from "react-awesome-reveal";
 const Featured = () => {
   return (
     <div className="my-10">
-      <h1 className="text-3xl font-bold divider divider-primary my-5">
+      <h1 className="text-3xl font-bold text-center text-primary my-12">
         Featured moments
       </h1>
       <div className="flex flex-wrap container mx-auto justify-center items-center gap-5">
-
-          
-         <img src="/images/1.webp" className="w-lg" alt="" />
-        <img src="/images/8.jpg"  className="w-lg" alt="" />
+        <img src="/images/1.webp" className="w-lg" alt="" />
+        <img src="/images/8.jpg" className="w-lg" alt="" />
         <img src="/images/12.jpg" className="w-lg " alt="" />
         <img src="/images/14.jpg" className="w-lg" alt="" />
-     
-       
       </div>
     </div>
   );
